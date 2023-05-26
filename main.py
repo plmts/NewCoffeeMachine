fee333a6f58f4b494225e3e078379cdb4b005e97
Paulo Matos <90912163+plmts@users.noreply.github.com>
2023-05-26 16:22:00 -0300

    some function and classes were implemented. Classes to get coffee machine resources and machine money balance.

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+## classes
+fazer_cafe = CoffeeMaker()
+dinheiro_maquina = MoneyMachine()
+menu = Menu()
+
+# código
+is_on = True
+
+while is_on:
+    bebidas = menu.get_items()
+    choice = input(f"What would you like? ({bebidas}):")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        fazer_cafe.report()
+        dinheiro_maquina.report()
+    else:
+        drink = menu.find_drink(choice)
+        print(drink)
+        
